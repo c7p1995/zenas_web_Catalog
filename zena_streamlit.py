7674a41fe1b61786412163c2b632076b49f27a20
c7p1995 <56692240+c7p1995@users.noreply.github.com>
2023-11-08 10:05:26 +0100

    Update zena_streamlit.py

--- zena_streamlit.py
+++ zena_streamlit.py
@@ -6,7 +6,16 @@ def get_color_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT color_or_style from catalog_for_website")
         return my_cur.fetchall()
+def get_image():
+        with my_cnx.cursor(a) as my_cur:
+        my_cur.execute("SELECT direct_url from catalog_for_website where color_or_style=a")
+        return my_cur.fetchall()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_data_row = get_fruit_load_list()
+my_data_row = get_color_list()
 my_cnx.close()
-fruits_selected=streamlit.select("Pick a sweatsuit colour or style:", list(my_data_row[color_or_style]),['Pink'])
+color_style_selected=stl.select("Pick a sweatsuit colour or style:", list(my_data_row[color_or_style]),['Pink'])
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_url = get_image(color_style_selected)
+my_cnx.close()
+st.markdown("![Alt Text]("+my_url+")")
