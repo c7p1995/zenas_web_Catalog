006b017b893e0e45ff7e3fb73866fa5ae0d5ba1d
c7p1995 <56692240+c7p1995@users.noreply.github.com>
2023-11-08 10:42:01 +0100

    Update zena_streamlit.py

--- zena_streamlit.py
+++ zena_streamlit.py
@@ -9,7 +9,7 @@ def get_color_list():
         return my_cur.fetchall()
 def get_image(a):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT direct_url from catalog_for_website where color_or_style='%a'")
+        my_cur.execute("SELECT direct_url from catalog_for_website where color_or_style=%a")
         return my_cur.fetchall()
 my_cnx = snowflake.connector.connect(**stl.secrets["snowflake"])
 my_data_row = pd.DataFrame(get_color_list())
